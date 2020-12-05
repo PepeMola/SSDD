0dbea57293e9801646371280c5c0b157a89fd9b6
PepeMola <joseantonio.arias1@alu.uclm.es>
2020-12-05 11:39:28 +0100

    Creacion DungeonClient y modificacion get_new_token. Modificaciones en AuthenticationClient.

--- AuthenticationClient.py
+++ AuthenticationClient.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# pylint: disable=W1203
+# pylint: disable=W0613
+
 import sys
+from sys import argv
 import Ice
 Ice.loadSlice('IceGauntlet.ice')
 # pylint: disable=E0401
@@ -11,43 +15,32 @@ import IceGauntlet
 import hashlib
 import getpass
 
-class Client(Ice.Application):
-    def run(self, argv): 
-        broker = self.communicator()
-        address = broker.stringToProxy(argv[1])
-        auth = IceGauntlet.AuthenticationPrx.checkedCast(address)
+class ClienteAutenticacion(Ice.Application):
     
+    def run(self, argv):
+        broker = self.communicator()
+        address = broker.stringToProxy(argv[3])
+        auth = IceGauntlet.AuthenticationPrx.checkedCast(address) #Interface Authentication --> IceGauntlet.ice
+
         if not auth:
             raise RuntimeError("Invalid proxy")
         
-        self.proxy = auth
-        print("Escriba su usuario: ")
-        user = input()
-        print(f"Escriba su contraseña: ")
-        passwordHash = getpass.getpass()
-        hashPass = hashlib.sha256(passwordHash.encode()).hexdigest()
-        print(hashPass)	
-        auth.changePassword(user, "Hola", hashPass)
-        token = auth.getNewToken(user, hashPass) 
-        print(hashPass)
-        auth.isValid(token)
-        return 0
+        user = argv[2]
+
+        print(user)
+        if argv[1] == '-t': # Obtener Token
+            actualPass = getpass.getpass()
+            sha_actualPass = hashlib.sha256(actualPass.encode()).hexdigest()
+            print(auth.getNewToken(user, sha_actualPass))
+
+        if argv[1] == '-p': # Setear contraseña default 
+            newPass = getpass.getpass()
+            sha_newPass = hashlib.sha256(newPass.encode()).hexdigest()
+            auth.changePassword(user, None, sha_newPass)
+
     
-    def isValid(self, token):
-        return self.proxy.isValid(token)
 
-    '''
-    def changePass(self, server, user, actualPass):
-        print("Please, type your new password:\n")
-        newPass = getpass.getpass()
-
-        server.changePassword(user, actualPass, newPass)
-        print("Password succesfully changed.\n")
-
-    def getToken(self, server, user, actualPass):
-        token = server.getNewToken(user, actualPass)
-        print(token)
-    '''
-
-
-sys.exit(Client().main(sys.argv))
+        return 0
+if __name__ == "__main__":
+    clienteAutenticacion = ClienteAutenticacion(argv)
+    sys.exit(ClienteAutenticacion().main(sys.argv)) 

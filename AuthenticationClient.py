22e8d04b9d95be8c4eaeede419ccad7b7fd64ebb
PepeMola <joseantonio.arias1@alu.uclm.es>
2020-12-06 17:06:23 +0100

    Nuevos cambios en AuthenticatioClient.py Añadidos scripts: run_map_server y run_game.

--- AuthenticationClient.py
+++ AuthenticationClient.py
@@ -15,26 +15,26 @@ import IceGauntlet
 
 class ClienteAutenticacion(Ice.Application):
     def run(self, argv):
-        broker = self.communicator()
-        address = broker.stringToProxy(argv[3])
-        auth = IceGauntlet.AuthenticationPrx.checkedCast(address)
+        broker=self.communicator()
+        address=broker.stringToProxy(argv[3])
+        auth=IceGauntlet.AuthenticationPrx.checkedCast(address)
         if not auth:
             raise RuntimeError("Invalid proxy")
-        user = argv[2]
+        user=argv[2]
         print(user)
-
+        actualPass=getpass.getpass()
+        sha_actualPass=hashlib.sha256(actualPass.encode()).hexdigest()
         # Obtener Token
-        if argv[1] == '-t':
-            actualPass = getpass.getpass()
-            sha_actualPass = hashlib.sha256(actualPass.encode()).hexdigest()
-            print(auth.getNewToken(user, sha_actualPass))
-
+        if argv[1]=='-t':
+            print(auth.getNewToken(user,sha_actualPass))
         # Setear contraseña default
-        if argv[1] == '-p':
-            newPass = getpass.getpass()
-            sha_newPass = hashlib.sha256(newPass.encode()).hexdigest()
-            auth.changePassword(user, None, sha_newPass)
+        if argv[1]=='-p':
+            newPass=getpass.getpass()
+            sha_newPass=hashlib.sha256(newPass.encode()).hexdigest()
+            auth.changePassword(user,sha_actualPass,sha_newPass)
+            print("Your password has been changed successfully.\n")
         return 0
+        
 if __name__ == "__main__":
     clienteAutenticacion = ClienteAutenticacion(argv)
     sys.exit(ClienteAutenticacion().main(sys.argv))
